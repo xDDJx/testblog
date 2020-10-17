626d301e09d1b0d9203ec27d7d6f7923d0ae5bbc
xDDJx <estddj@gmail.com>
2020-10-17 20:38:47 +1000

    Add project

---
+++ src/main/java/com/example/blog/controllers/MainController.java
@@ -0,0 +1,22 @@
+package com.example.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String greeting(Model model) {
+        model.addAttribute("title", "Главная страница");
+        return "home";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "Обо мне");
+        return "about";
+    }
+
+}
